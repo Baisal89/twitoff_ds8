a2145232945c245e399b8d3affae6256ef8cbc9d
baisal89 <baisal89@user.noreply.github.com>
2019-11-13 09:50:31 -0500

    modified

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from .models import DB
 
 #now we make a app factory
 
 def create_app():
     app = Flask(__name__)
 
+    #add out config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+    #now have the database know abount the app
+
+    DB.init_app(app)
+
 
     @app.route('/')
     def root():
         return "Wlcome to Twitoff"
 
-    return app 
+    return app
---
+++ TWITOFF/models.py
@@ -0,0 +1,18 @@
+"""These are my database models"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# import database. capital for gloval scope
+
+DB = SQLAlchemy()
+
+class User(DB.Model):
+    """Twitter users that we analyze"""
+    is = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+
+
+class Tweet(DB.Model):
+    """The user's tweets form twitter"""
+    id = DB.Column(DB.Integer, primary_key=True)
+     text=DB.Column(DB.Unicode(200))
