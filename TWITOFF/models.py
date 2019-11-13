77f9c098cd94d75a944f23977372c8174ac2f4ab
baisal89 <baisal89@user.noreply.github.com>
2019-11-13 16:38:51 -0500

    midfied app to see database

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -8,11 +8,11 @@ DB = SQLAlchemy()
 
 class User(DB.Model):
     """Twitter users that we analyze"""
-    is = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
 
 
 class Tweet(DB.Model):
     """The user's tweets form twitter"""
     id = DB.Column(DB.Integer, primary_key=True)
-     text=DB.Column(DB.Unicode(200))
+    text=DB.Column(DB.Unicode(200))
